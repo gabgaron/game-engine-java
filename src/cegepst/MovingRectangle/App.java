19a0a6e5f5611a5f44c0302507e6ff7d26e419ad
gabri <gabrielcotegaron123@gmail.com>
2021-10-22 12:00:49 -0400

    Création jeu de rectangle

---
+++ src/cegepst/MovingRectangle/App.java
@@ -0,0 +1,7 @@
+package cegepst.MovingRectangle;
+
+public class App {
+    public static void main(String[] args) {
+        (new MovingRectangleGame()).start();
+    }
+}
---
+++ src/cegepst/MovingRectangle/MovingRectangleGame.java
@@ -0,0 +1,27 @@
+package cegepst.MovingRectangle;
+
+import cegepst.engine.Buffer;
+import cegepst.engine.Game;
+
+public class MovingRectangleGame extends Game {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Buffer buffer) {
+
+    }
+
+    @Override
+    public void conclude() {
+
+    }
+}
