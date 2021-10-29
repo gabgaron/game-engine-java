e1126e88194f5967349900e7363715a67717897f
gabri <gabrielcotegaron123@gmail.com>
2021-10-29 12:26:13 -0400

    creation d'un nouveau jeu tank qui tire des missiles

--- src/cegepst/MovingRectangle/GamePad.java
+++ src/cegepst/MovingRectangle/GamePad.java
@@ -7,12 +7,18 @@ import java.awt.event.KeyEvent;
 public class GamePad extends MovementController {
 
     private int quitKey = KeyEvent.VK_Q;
+    private int fireKey = KeyEvent.VK_SPACE;
 
     public GamePad() {
         bindKey(quitKey);
+        bindKey(fireKey);
     }
 
     public boolean isQuitPressed() {
         return isKeyPressed(quitKey);
+    }
+
+    public boolean isFirePressed() {
+        return isKeyPressed(fireKey);
     }
 }
