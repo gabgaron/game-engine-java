96a658d88da272d805b28a6a1687e0facd59939f
gabri <gabrielcotegaron123@gmail.com>
2021-11-05 11:44:55 -0400

    intégration de bordude du monde

--- src/cegepst/viking/World.java
+++ src/cegepst/viking/World.java
@@ -5,14 +5,31 @@ import cegepst.engine.Buffer;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class World {
 
     private static final String MAP_PATH = "images/demo.png";
     private Image background;
+    private ArrayList<Blockade> worldBorders;
+
+    public World() {
+        worldBorders = new ArrayList<>();
+        Blockade topBorder = new Blockade();
+        topBorder.teleport(0, 0);
+        topBorder.setDimension(400, 48);
+        Blockade topBorder2 = new Blockade();
+        topBorder2.setDimension(400, 48);
+        topBorder2.teleport(464, 0);
+        worldBorders.add(topBorder);
+        worldBorders.add(topBorder2);
+    }
 
     public void draw(Buffer buffer) {
         buffer.drawImage(background, 0, -64);
+        for (Blockade blockade : worldBorders) {
+            blockade.draw(buffer);
+        }
     }
 
     //todo class loader --> refactor
